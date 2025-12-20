611f38ea822bf3ae719956b54c45efc008db982f
PatatoBit <pathonscript@gmail.com>
2025-12-20 13:57:29 +0700

    Initial Commit

---
+++ AirEye/AirEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirEyeApp.swift
+//  AirEye
+//
+//  Created by Patato on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AirEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
